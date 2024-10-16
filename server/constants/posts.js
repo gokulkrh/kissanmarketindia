79b91aeac72a8070ad0e5ac28363bbc95796a45c
Gokul Krishnan G <gokulkrh@gmail.com>
2024-10-16 05:41:26 +0530

    minor changes

--- server/constants/posts.js
+++ server/constants/posts.js
@@ -1,13 +1,13 @@
 const categories = {
-  vegetablesAndFruits: "Vegetable & Fruits",
-  rootVegetables: "Root Vegetables",
-  grains: "Grains",
-  animalsAndPets: "Animals & Pets",
-  honeyFarmerProducts: "Honey Farmer Products",
-  fishFarmerProducts: "Fish Farmer Products",
-  vanavasiAndHandicraft: "Vanavasi & Handicraft",
-  pantsAndSeeds: "Plants & Seeds",
-  otherProducts: "Other Products",
+	vegetablesAndFruits: "Vegetable & Fruits",
+	rootVegetables: "Root Vegetables",
+	grains: "Grains",
+	animalsAndPets: "Animals & Pets",
+	honeyFarmerProducts: "Honey Farmer Products",
+	fishFarmerProducts: "Fish Farmer Products",
+	vanavasiAndHandicraft: "Vanavasi & Handicraft",
+	pantsAndSeeds: "Plants & Seeds",
+	otherProducts: "Other Products",
 };
 
 export { categories };

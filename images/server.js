f7e4c1a0fa4fe9ece4b23e703f345923a4247c9d
Gokul Krishnan G <gokulkrh@gmail.com>
2024-11-01 03:49:56 +0530

    docker setup

--- images/server.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-
-const app = express();
-app.use(express.static("./"));
-const PORT = process.env.PORT || 8000;
-
-app.listen(PORT, console.log("server is running"));
